59517498ce40340bd99f949d91e6c14b50dcd2df
Dronzer2code <suparnamaity1985iii@gmail.com>
2025-10-18 01:37:46 +0530

    fix

--- old/drawer.js
+++
@@ -1,6 +0,0 @@
-function toggleDrawer() {
-    const drawer = document.getElementById('infoDrawer');
-    drawer.classList.toggle('open');
-    const arrow = drawer.querySelector('.drawer-arrow');
-    arrow.textContent = drawer.classList.contains('open') ? '▼' : '▲';
-}
